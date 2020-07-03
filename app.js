6b9cba7966129b70805ae599ec8e3202ee0d01ed
Chen Eini <chen.eini000@gmail.com>
2020-07-03 17:14:50 +0300

    update public files

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
@@ -23,4 +23,22 @@ app.use('/users', usersRouter);
 articlesRouter(app);
 categoriesRouter(app);
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    console.log('url not found');
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    const error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json(error);
+});
+
 module.exports = app;
+
+
